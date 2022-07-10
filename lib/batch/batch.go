7d938fd5de40fac19aaf1dea1d859e608e32c04d
CODEINUS59 <alexvad1488@gmail.com>
2022-07-10 17:56:56 +0300

    add solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,11 +1,26 @@
 package batch
 
 import (
+	"log"
+	"sync"
 	"time"
 )
 
 type user struct {
 	ID int64
+}
+
+//add user into list
+func addUser(list *[]user, ch <-chan int64, wg *sync.WaitGroup, mx *sync.Mutex) {
+	defer wg.Done()
+
+	for num := range ch {
+		user := getOne(num)
+		mx.Lock()
+		*list = append(*list, user)
+		mx.Unlock()
+	}
+
 }
 
 func getOne(id int64) user {
@@ -14,5 +29,32 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var (
+		userList []user
+		wg       sync.WaitGroup
+		mx       sync.Mutex
+	)
+
+	//it`s unuseful: add more theads than users`
+	if pool > n {
+		log.Fatal("pool cannot be bigger than count of users!")
+	}
+
+	nChan := make(chan int64)
+
+	//init goroutines
+	wg.Add(int(pool))
+	for i := 1; i <= int(pool); i++ {
+		go addUser(&userList, nChan, &wg, &mx)
+	}
+
+	//write into channel
+	for j := 0; j < int(n); j++ {
+		nChan <- int64(j)
+	}
+	close(nChan)
+
+	wg.Wait()
+
+	return userList
 }
